1d6e6ac325c0e11908f965b4d8019a3c9bf4b51f
Vincent Lee <vlee@vlee.me.uk>
2020-06-17 15:48:22 +0100

    minor title change for embed

--- cogs/Roles.py
+++ cogs/Roles.py
@@ -79,7 +79,7 @@ class Roles(commands.Cog):
                     captainNotAssigned = captainNotAssigned + "- {} | {}\n".format(x, teamNames[x])
                 captainNotAssigned = captainNotAssigned + "```"
                 # Add field to embed
-                embed.add_field(name="List of captains that failed to be assigned",
+                embed.add_field(name="List of captains that failed to be assigned:",
                                 value=captainNotAssigned, inline=False)
             await replyChannel.send(embed=embed)  # send embed
             print("Updated captain role for {} at {}".format(serverID, datetime.datetime.utcnow()))
