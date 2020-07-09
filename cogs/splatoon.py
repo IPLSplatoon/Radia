f5b7d37db0c9c31e3786350f469aba9318fb588b
Vincent Lee <vlee@vlee.me.uk>
2020-07-09 01:42:05 +0100

    Document command prams

--- cogs/splatoon.py
+++ cogs/splatoon.py
@@ -10,8 +10,15 @@ class Splatoon(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='generateSwiss', help="Generate a swiss map set", aliases=["generateswiss"])
-    async def generate_swiss(self, ctx, maps, rounds, bestOf, info=None,
+    @commands.command(name='generateSwiss', help="Generate a Swiss map set\n"
+                                                 "<maps>: List of maps you want to have."
+                                                 "Maps must be placed within speech marks & use commas to split maps."
+                                                 "<rounds>: Number of rounds to generate\n"
+                                                 "<bestOf>: Number of games per round.\n"
+                                                 "[Embed Title]: Optional title for the embed\n"
+                                                 "[info]: Optional Info for the embed\n"
+                                                 "[Maps]: Optional, limit modes used.", aliases=["generateswiss"])
+    async def generate_swiss(self, ctx, maps, rounds, bestOf, embedTitle="Swiss Map List", info=None,
                              modes='Splat Zones,Tower Control,Rainmaker,Clam Blitz'):
         with ctx.typing():
             modes = modes.split(",")
@@ -27,17 +34,24 @@ class Splatoon(commands.Cog):
                 return
             mapsList = await utils.generate_swiss(int(rounds), int(bestOf), maps, modes)
             if mapsList:
-                embed = await utils.create_embed("Swiss Map List", info)
+                embed = await utils.create_embed(embedTitle, info)
                 for x in range(len(mapsList)):
-                    embed.add_field(name="Round {}".format(x+1), value="```\n{}```".format(mapsList[x]), inline=False)
+                    embed.add_field(name="Round {}".format(x + 1), value="```\n{}```".format(mapsList[x]), inline=False)
                 await ctx.send(embed=embed)
             else:
                 embed = await utils.create_embed("Generate Swiss Error", "Unable generate set")
                 await ctx.send(embed=embed)
 
-    @commands.command(name='generateTopCut', help="Generate a swiss map set", aliases=["generatetopcut"])
-    async def generate_top_cut(self, ctx, maps, rounds, bestOf, info=None,
-                             modes='Splat Zones,Tower Control,Rainmaker,Clam Blitz'):
+    @commands.command(name='generateTopCut', help="Generate a Top Cut map set\n"
+                                                  "<maps>: List of maps you want to have.\n"
+                                                  "Maps must be placed within speech marks & use commas to split maps."
+                                                  "<rounds>: Number of rounds to generate\n"
+                                                  "<bestOf>: Number of games per round.\n"
+                                                  "[Embed Title]: Option title for the embed\n"
+                                                  "[info]: Option Info for the embed\n"
+                                                  "[Maps]: Options, limit maps used.", aliases=["generatetopcut"])
+    async def generate_top_cut(self, ctx, maps, rounds, bestOf, embedTitle="Top Cut Map List", info=None,
+                               modes='Splat Zones,Tower Control,Rainmaker,Clam Blitz'):
         with ctx.typing():
             modes = modes.split(",")
             maps = maps.split(",")
@@ -52,9 +66,9 @@ class Splatoon(commands.Cog):
                 return
             mapList = await utils.generate_top_cut(int(rounds), int(bestOf), maps, modes)
             if mapList:
-                embed = await utils.create_embed("Top Cut Map List", info)
+                embed = await utils.create_embed(embedTitle, info)
                 for x in range(len(mapList)):
-                    embed.add_field(name="Round {}".format(x+1), value="```\n{}```".format(mapList[x]), inline=False)
+                    embed.add_field(name="Round {}".format(x + 1), value="```\n{}```".format(mapList[x]), inline=False)
                 await ctx.send(embed=embed)
             else:
                 embed = await utils.create_embed("Generate Top Cut Error", "Unable generate set")
