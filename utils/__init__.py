4c82bb69a8bbe916ea7b0d76924057f872acb671
LeptoFlare <leptoflare@gmail.com>
2020-08-18 18:22:38 -0400

    rename ReactRoleDB > react_role

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
 from .embedder import create_embed
 from .error_collector import error
-from .reactRoleDB import RoleReactList
+from .react_role import RoleReactList
 from .splatoonSetGen import generate_swiss, generate_top_cut
