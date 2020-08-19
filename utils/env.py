f5a7ca41aa60fc2238ec8eee1a60d2d670930ee7
LeptoFlare <leptoflare@gmail.com>
2020-08-19 19:09:56 -0400

    create env.py to centralize environment

---
+++ utils/env.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+import gSheetConnector
+import DBConnector
+import battlefyConnector
+
+load_dotenv("files/.env")
+
+class Env:
+
+    def __init__(self):
+        self.low_ink_discord_token = os.environ.get("low_ink_discord_token")
+        self.google_sheet_name = os.environ.get("google_sheet_name")
+        self.DB_String = os.environ.get("DB_String")
+        self.system_environment = os.environ.get("system_environment")
+
+        self.gsheet = gSheetConnector.SheetConnector("files/googleAuth.json", self.google_sheet_name)
+        self.db = DBConnector.DBConnect(self.DB_String)
+        self.battlefy = battlefyConnector.BattlefyUtils()
+
+
+env = Env()
