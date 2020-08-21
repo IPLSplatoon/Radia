6feb7eb1c9b84528751276d3395bc14a3f1d94f1
LeptoFlare <leptoflare@gmail.com>
2020-08-21 19:51:57 -0400

    Add base __init__.py

---
+++ radia/__init__.py
@@ -0,0 +1,11 @@
+"""
+Initalizes the early stuff for the entire application
+
+This includes:
+ - Setting up logging
+ - Optionally setting up sentry
+ - Opening a database connection
+ - etc.
+"""
+
+import os
--- radia/bot.py
+++ radia/bot.py
@@ -14,3 +14,12 @@ class Bot(commands.Bot):
 
     async def on_ready(self):
         logging.info(f"Logged in as: {self.user.name}")
+
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(f"Missing data, you got got to enter something after the command!\nYou can use `{self.command_prefix}help` for help.")
+        elif isinstance(error, (commands.CommandNotFound, commands.MissingRole)):
+            return
+        else:
+            logging.error(error)
+
