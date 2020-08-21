f9227a6523361189c412757d7449d3e5cd06bfed
LeptoFlare <leptoflare@gmail.com>
2020-08-21 19:52:10 -0400

    Initialize logger in __init__.py

--- radia/__init__.py
+++ radia/__init__.py
@@ -9,3 +9,17 @@ This includes:
 """
 
 import os
+import logging
+
+logging.basicConfig(
+    level=(
+        logging.DEBUG if os.getenv("DEBUG") else logging.INFO
+    ),
+    format='\033[31m%(levelname)s\033[0m \033[90min\033[0m \033[33m%(filename)s\033[0m \033[90mat\033[0m %(asctime)s\033[90m:\033[0m %(message)s',
+    datefmt='\033[32m%m/%d/%Y\033[0m \033[90mon\033[0m \033[32m%H:%M:%S\033[0m'
+)
+logging.getLogger("discord").setLevel(logging.ERROR)
+logging.getLogger("websockets").setLevel(logging.WARNING)
+logging.getLogger("asyncio").setLevel(logging.WARNING)
+
+logging.getLogger(__name__)
