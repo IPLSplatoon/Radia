fb1a02f797bfa0aab42ab9e20bbf22c897e8dd77
LeptoFlare <leptoflare@gmail.com>
2020-08-21 19:54:07 -0400

    Load cogs in __main__.py

--- radia/__main__.py
+++ radia/__main__.py
@@ -4,7 +4,23 @@ Initalizes the bot
 This includes importing the bot, loading the cogs, setting the prefix, etc.
 """
 
+import os
+import logging
+
+from radia import cogs
 from radia.bot import Bot
+
+# Create Bot
 bot = Bot(command_prefix="!")
 
+# Load Cogs
+for cog in cogs.names:
+    try:
+        bot.load_extension("radia.cogs." + cog)
+        logging.info("Loaded cog: %s", cog)
+    except Exception as e:
+        logging.warn("Failed to load cog: %s", cog)
+        logging.error(type(e).__name__, e)
+
+# Run Bot
 bot.run(os.getenv("TOKEN"))
