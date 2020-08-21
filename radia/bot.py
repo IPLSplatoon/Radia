d1809d1b23f525605cae6115528933250d873454
LeptoFlare <leptoflare@gmail.com>
2020-08-21 19:53:20 -0400

    Create update_presence task loop the on_command_error was in some other commit by accident but I'm too lazy to go back and fix that so have fun finding it

--- radia/bot.py
+++ radia/bot.py
@@ -14,6 +14,7 @@ class Bot(commands.Bot):
 
     async def on_ready(self):
         logging.info(f"Logged in as: {self.user.name}")
+        self.update_presence.start()
 
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
@@ -23,3 +24,23 @@ class Bot(commands.Bot):
         else:
             logging.error(error)
 
+    @tasks.loop(seconds=60)
+    async def update_presence(self):
+        await self.change_presence(activity=discord.Game(random.choice([
+            "Powered by High Ink?",
+            "Signup for Low Ink!",
+            "The Low Ink bot.",
+            "!help to get started",
+            "Sprinkles!",
+            "what is luti?",
+            "Round 4, here we go again!",
+            "What is Low Ink?",
+            "Ban Kraken Mare",
+            "Icon by Ozei#3125",
+            "Wawa!",
+            "Stream @ Twitch.tv/inkfarer",
+            "According to all known laws of aviation",
+            "I kid you not Hoeen, he turns himself into a pickle.",
+            "Go to sleep Lepto",
+            "Skye passed out again",
+        ])))
