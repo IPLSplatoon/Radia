a37a611e35e5dc1017408643c1f3b4e40264fd28
LeptoFlare <leptoflare@gmail.com>
2020-08-21 20:19:03 -0400

    Port the embedder

---
+++ radia/utils/embed.py
@@ -0,0 +1,30 @@
+import discord
+
+async def create(**kwargs) -> discord.Embed:
+    """
+    Create a discord embed template
+    :return: discord.embed
+        discord.py embed object
+    """
+    embed = discord.Embed(color=0xe0fe3a, **kwargs)
+    embed.set_author(
+        name="Radia",
+        url="https://battlefy.com/low-ink",
+        icon_url="https://cdn.vlee.me.uk/LowInk/LowInkIcon.png")
+    embed.set_footer(text="The Low Ink Bot", icon_url="https://cdn.vlee.me.uk/LowInk/LowInk.png")
+    embed.timestamp = datetime.datetime.utcnow()
+    return embed
+
+async def listblock(items: list) -> str:
+    """
+    Return a formatted list
+    :param list items:
+        List of items to format
+    :return str:
+        The list codeblock
+    """
+    return "\n".join([
+        "```",
+        *[f"- {item}" for item in items],
+        "```"
+    ])
