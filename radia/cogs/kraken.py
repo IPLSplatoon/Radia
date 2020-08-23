f71f248b251550804dc510fb04c5d747059d897c
LeptoFlare <leptoflare@gmail.com>
2020-08-22 22:23:09 -0400

    Fix bug with kraken cog

--- radia/cogs/kraken.py
+++ radia/cogs/kraken.py
@@ -19,8 +19,7 @@ class Kraken(commands.Cog):
         if guild is None:
             return logging.warn("Cannot run update_roles, is the bot in the Low Ink server?")
         kraken = discord.utils.get(guild.members, id=158733178713014273)
-
-        await kraken.remove_roles(*[
+        role_ids = [
             "689622040222761058", "471466333771399168", "563484622717976606",
             "722500918485975040", "717481862242762793", "717476155590180876",
             "717475987821953085", "406171863698505739", "406160013531283457",
@@ -28,7 +27,8 @@ class Kraken(commands.Cog):
             "689159249200283694", "724997028291280896", "717475987821953085",
             "717476155590180876", "726243712908263484", "726961896489484339",
             "726904603756462080", "726904633720832100", "725146685684056097"
-        ])
+        ]
+        await kraken.remove_roles(*[guild.get_role(role_id) for role_id in role_ids])
 
 
 def setup(bot):
