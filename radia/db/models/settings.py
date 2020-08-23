dfaaf6ed9660cfc80b5edcb0fd6a38453572b62a
LeptoFlare <leptoflare@gmail.com>
2020-08-22 23:59:14 -0400

    Add settings model

---
+++ radia/db/models/settings.py
@@ -0,0 +1,30 @@
+"""Stores the settings table model."""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Boolean
+
+Base = declarative_base()
+
+
+class Settings(Base):
+    """Settings table model."""
+
+    __tablename__ = "settings"
+    server = Column(
+        String(), primary_key=True, unique=True, nullable=False
+    )
+    captain_role = Column(
+        String(), unique=True
+    )
+    bot_channel = Column(
+        String(), unique=True
+    )
+    battlefy_field = Column(
+        String()
+    )
+    battlefy_tourney = Column(
+        String()
+    )
+    auto_assign_captain_role = Column(
+        Boolean()
+    )
