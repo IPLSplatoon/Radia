200bc7a1decfc2e44da938bf7bf5ea1304576656
LeptoFlare <leptoflare@gmail.com>
2020-08-22 23:10:02 -0400

    Fix the database string haha I didn't test it again oooop

--- radia/db/__init__.py
+++ radia/db/__init__.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import sessionmaker
 
 class Connector:
     def __init__(self):
-        self.engine = create_engine(f"db://postgres:{os.getenv('POSTGRES')}@localhost:8080")
+        self.engine = create_engine(f"postgresql://postgres:{os.getenv('POSTGRES')}@db:5432")
         self.sessionmaker = sessionmaker(bind=self.engine)
         self.session = self.sessionmaker()
         logging.info("Loaded DB Connector: Session Created")
