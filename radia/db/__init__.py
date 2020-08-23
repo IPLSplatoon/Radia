43545561de2311d6955c75620b26e7c2375bf49d
LeptoFlare <leptoflare@gmail.com>
2020-08-22 23:26:02 -0400

    Add database connector docstring

--- radia/db/__init__.py
+++ radia/db/__init__.py
@@ -8,6 +8,8 @@ from sqlalchemy.orm import sessionmaker
 
 
 class Connector:
+    """Database connector."""
+
     def __init__(self):
         self.engine = create_engine(f"postgresql://postgres:{os.getenv('POSTGRES')}@db:5432")
         self.sessionmaker = sessionmaker(bind=self.engine)
