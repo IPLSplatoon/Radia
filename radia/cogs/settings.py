d36ce1027cec0d8008d5876837a2a167f111c4b5
LeptoFlare <leptoflare@gmail.com>
2020-08-24 20:37:25 -0400

    Update how the settings embed is displayed

--- radia/cogs/settings.py
+++ radia/cogs/settings.py
@@ -23,8 +23,8 @@ class Settings(commands.Cog):
             try:
                 server = session.query(SettingsModel).filter(SettingsModel.server == str(ctx.guild.id)).one()
                 embed = utils.embed.create(title=f"Current settings for {str(ctx.guild)}:")
-                embed.add_field(name="Captain Role:", value=ctx.guild.get_role(int(server.captain_role)).mention, inline=False)
-                embed.add_field(name="Bot Channel:", value=ctx.guild.get_channel(int(server.bot_channel)).mention, inline=False)
+                embed.add_field(name="Captain Role:", value=ctx.guild.get_role(int(server.captain_role)).mention)
+                embed.add_field(name="Bot Channel:", value=ctx.guild.get_channel(int(server.bot_channel)).mention)
                 embed.add_field(name="Battlefy Field:", value=server.battlefy_field, inline=False)
                 embed.add_field(name="Battlefy Tourney:", value=server.battlefy_tourney, inline=False)
                 embed.add_field(name="Auto-assign Captain Role:", value=utils.embed.emojibool(server.auto_assign_captain_role), inline=False)
@@ -57,6 +57,63 @@ class Settings(commands.Cog):
             else:
                 await ctx.send(f"Settings have already been initialized for your server, you can view them with `{ctx.prefix}settings`.")
 
+    @settings.group(aliases=["update"])
+    async def edit(self, ctx):
+        """Edit a field in the settings."""
+
+    @edit.command(aliases=["captain", "role"])
+    async def captain_role(self, ctx, mention):
+        """Edit the captain role."""
+        with db.connector.open() as session:
+            try:
+                server = session.query(SettingsModel).filter(SettingsModel.server == str(ctx.guild.id)).one()
+            except NoResultFound:
+                await ctx.send(f"There are no settings for your server. initialize your server with `{ctx.prefix}`settings init`")
+            else:
+                if mentions := ctx.message.role_mentions:
+                    server.captain_role = mentions[0].id
+                    await ctx.send("Successfully changed field.")
+                else:
+                    await ctx.send("Invalid captain role, did you remember to mention the role?")
+
+    @edit.command(aliases=["bot", "bot_channel"])
+    async def channel(self, ctx, mention):
+        """Edit the bot channel."""
+        with db.connector.open() as session:
+            try:
+                server = session.query(SettingsModel).filter(SettingsModel.server == str(ctx.guild.id)).one()
+            except NoResultFound:
+                await ctx.send(f"There are no settings for your server. initialize your server with `{ctx.prefix}`settings init`")
+            else:
+                if mentions := ctx.message.channel_mentions:
+                    server.bot_channel = mentions[0].id
+                    await ctx.send("Successfully changed field.")
+                else:
+                    await ctx.send("Invalid bot channel, did you remember to mention the channel?")
+
+    @edit.command(aliases=["field"])
+    async def battlefy_field(self, ctx, value):
+        """Edit battlefy field."""
+        with db.connector.open() as session:
+            try:
+                server = session.query(SettingsModel).filter(SettingsModel.server == str(ctx.guild.id)).one()
+            except NoResultFound:
+                await ctx.send(f"There are no settings for your server. initialize your server with `{ctx.prefix}`settings init`")
+            else:
+                server.battlefy_field = value
+                await ctx.send("Successfully changed field.")
+    
+    @edit.command(aliases=["tourney"])
+    async def battlefy_tourney(self, ctx, value):
+        """Edit battlefy tournament."""
+        with db.connector.open() as session:
+            try:
+                server = session.query(SettingsModel).filter(SettingsModel.server == str(ctx.guild.id)).one()
+            except NoResultFound:
+                await ctx.send(f"There are no settings for your server. initialize your server with `{ctx.prefix}`settings init`")
+            else:
+                server.battlefy_tourney = value
+                await ctx.send("Successfully changed field.")
 
 def setup(bot):
     bot.add_cog(Settings(bot))
