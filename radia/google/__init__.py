03aedf145416899ff08e2ab9cb1001c874861f3a
LeptoFlare <leptoflare@gmail.com>
2020-08-27 01:25:37 -0400

    Load .googleenv.json file as creds Authorization is to be implemented

--- radia/google/__init__.py
+++ radia/google/__init__.py
@@ -7,5 +7,19 @@ import logging
 class Connector:
     """Database connector."""
 
+    def __init__(self):
+        try:
+            with open(".googleenv.json") as infile:
+                creds = json.load(infile)
+
+        except FileNotFoundError:
+            logging.error(".googleenv.json - file not found.")
+            raise EnvironmentError
+
+        else:
+            pass  # Authorization
+
+        logging.debug("Loaded google.connector")
+
 
 connector = Connector()
