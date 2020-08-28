629774f27073e1ee8719367755d761cfb41d83b9
LeptoFlare <leptoflare@gmail.com>
2020-08-28 19:18:05 +0000

    Create a Responses subclass of Worksheet and... rename records to dataframe Create options and get functions for responses sheets

--- radia/google/worksheet.py
+++ radia/google/worksheet.py
@@ -8,10 +8,25 @@ class Worksheet:
 
     def __init__(self, gsheet, name):
         self.gsheet = gsheet
-        self.worksheet = self.gsheet.worksheet("Rules")
-        self.records = pd.DataFrame(self.worksheet.get_all_records())
-    
+        self.name = name
+        self.worksheet = self.gsheet.worksheet(self.name)
+        self.dataframe = pd.DataFrame(self.worksheet.get_all_records())
+
     async def refresh(self):
         """Refresh the worksheet and records by reinitializing them."""
-        self.worksheet = self.gsheet.worksheet("Rules")
-        self.records = pd.DataFrame(self.worksheet.get_all_records())
+        self.worksheet = self.gsheet.worksheet(self.name)
+        self.dataframe = pd.DataFrame(self.worksheet.get_all_records())
+
+
+class Responses(Worksheet):
+    """Represents a google sheets worksheet with prefixes and responses."""
+
+    def options(self):
+        """Return the rules options."""
+        return self.dataframe["prefix0"]
+    
+    def get(self, prefix):
+        """Return the rules section with the given prefix."""
+        for i, row in self.dataframe.iterrows():
+            if prefix in [p for p in row[:5] if p != '']:
+                return row["prefix0"], row["Response"]
