2f02e373a3c9627435e0212498b79b250b548730
LeptoFlare <leptoflare@gmail.com>
2020-08-28 19:18:27 +0000

    Create rules command

--- radia/cogs/info.py
+++ radia/cogs/info.py
@@ -19,8 +19,21 @@ class Info(commands.Cog):
         await ctx.message.add_reaction("\u2728")
 
     @commands.command(aliases=["rule"])
-    async def rules(self, ctx, rules=None, image=False):
-        pass
+    async def rules(self, ctx, prefix=None, image: bool = False):
+        if prefix:
+            try:
+                name, response = google.connector.rules.get(prefix)
+                embed = utils.embed.create(title=f"{name.capitalize()} Rules", description=response)
+                await ctx.send(embed=embed)
+            except TypeError:
+                await ctx.send("Section could not be found, try a different prefix.")
+
+        else:
+            embed = utils.embed.create(title="Rules")
+            embed.add_field(
+                name="Options:",
+                value=utils.embed.listblock(google.connector.rules.options()))
+            await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Info(bot))
