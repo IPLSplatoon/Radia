21c088ce9c281d27e3f07760e321c6ec656dd354
LeptoFlare <leptoflare@gmail.com>
2020-08-28 19:37:38 +0000

    Create whatis command

--- radia/cogs/info.py
+++ radia/cogs/info.py
@@ -37,5 +37,25 @@ class Info(commands.Cog):
                 value=utils.embed.listblock(google.connector.rules.options()))
             await ctx.send(embed=embed)
 
+    @commands.command(aliases=["canned"])
+    async def whatis(self, ctx, category=None, image: bool = False):
+        if category:
+            try:
+                name, response, image_link = google.connector.whatis.get(category.lower())
+                embed = utils.embed.create(title=f"What Is... {name.capitalize()}?", description=response)
+                if image:
+                    embed.set_image(image_link)
+                await ctx.send(embed=embed)
+            except TypeError:
+                await ctx.send("Section could not be found, try a different category.")
+
+        else:
+            embed = utils.embed.create(title="What Is...")
+            embed.add_field(
+                name="Options:",
+                value=utils.embed.listblock(google.connector.whatis.options()))
+            await ctx.send(embed=embed)
+
+
 def setup(bot):
     bot.add_cog(Info(bot))
