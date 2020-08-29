77666fff6cc00391c2ac5f1fccb77914ec3c43d8
LeptoFlare <leptoflare@gmail.com>
2020-08-29 00:36:27 +0000

    Create tournament model

---
+++ radia/db/models/tournament.py
@@ -0,0 +1,33 @@
+"""Stores the tournament table model."""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Boolean
+
+from radia.db import connector
+
+Base = declarative_base()
+
+
+class Tournament(Base):
+    """Tournament table model."""
+
+    __tablename__ = "tournament"
+    id = Column(
+        Integer, primary_key=True, unique=True, nullable=False)
+
+    battlefy = Column(
+        String, unique=True)
+    
+    date = Column(
+        DateTime)
+    
+    guild = Column(
+        String)
+    
+    role = Column(
+        String)
+    
+    teams = relationship("TournamentTeam")
+
+
+Base.metadata.create_all(connector.engine)
