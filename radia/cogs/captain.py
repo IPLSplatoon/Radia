28c414c50c10ef1287e24c8ee62fb4384329e4b8
LeptoFlare <leptoflare@gmail.com>
2020-09-04 23:00:50 +0000

    Create captain command group and check command Add helper in_server function

--- radia/cogs/captain.py
+++ radia/cogs/captain.py
@@ -3,8 +3,7 @@
 import discord
 from discord.ext import commands
 
-from radia import utils, battlefy
-from radia import utils
+from radia import utils, battlefy#, db
 
 
 class Captain(commands.Cog):
@@ -12,14 +11,47 @@ class Captain(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.member = commands.MemberConverter()
 
     @commands.group(invoke_without_subcommand=True)
-    async def captain(self, ctx):
+    async def captain(self, ctx, *args, **kwargs):
         """
         Show the current status of captains.
         Group of commands handling the captain roles.
         """
+        await ctx.invoke(self.check, *args, **kwargs)  # Run 'captain check' command
+
+    @captain.command()
+    async def check(self, ctx):
+        """Show the current status of captains."""
+        # settings = db.connector.find_settings(server=ctx.guild.id)
+
+        teams = await battlefy.connector.get_teams("5f21e5e1f5fc96423c53d094") # DB: settings.tournament
+
+        # Create list of invalid captains
+        invalid_captains = [
+            f"{team.captain.discord} | {team.name}"
+            for team in teams
+            if not self.in_server(ctx, team.captain.discord)
+        ]
+
+        # Send Status Check
+        embed = utils.embed.create(title="Captain Check", description="Here's a quick status check on captains.")
+        embed.add_field(name="\ufeff", value="\n".join([
+            f"Total Teams: `{len(teams)}`",
+            f"Invalid Captains: `{len(invalid_captains)}"
+        ]))
+        embed.add_field(name="List of Invalid Captains:", value=utils.embed.list_block(invalid_captains))
+        await ctx.send(embed=embed)
         """Group of commands handling the captain roles."""
+    
+    async def in_server(self, ctx, member: str) -> bool:
+        """Check if any string representation of a member is in the server or not."""
+        try:
+            self.member.convert(ctx, member)
+        except commands.BadArgument:
+            return False
+        return True
 
 
 def setup(bot):
