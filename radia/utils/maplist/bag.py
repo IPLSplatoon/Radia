1fcf473ff514070a7103a693bae53fc0ff79a9aa
LeptoFlare <leptoflare@gmail.com>
2020-09-10 18:51:30 +0000

    Create a Bag class to modularize balanced random picking

---
+++ radia/utils/maplist/bag.py
@@ -0,0 +1,27 @@
+"""Contains Bag class."""
+
+import random
+
+
+class Bag:
+    """ Implemented algorithm to create balanced random picking.
+    :param maximum:
+        The maximum number of recents in the bag at a time.
+        A higher number means more balanced but less randomized.
+    """
+    def __init__(self, items, maximum):
+        self.items = items
+        self.max = maximum
+        self.recents = []
+    
+    def pick(self, prune=True):
+        pick = random.choice(self.items - self.recents)
+        self.recents.append(pick)
+        if prune:
+            self.prune()
+        return pick
+
+    def prune(self):
+        """Remove the oldest elements from the bag out of the maximum."""
+        if len(self.recents) >= self.max:
+            self.recents = self.recents[-self.max:]
