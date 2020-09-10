f1c9cfef009d2568610d47d08022b1161b568ce5
LeptoFlare <leptoflare@gmail.com>
2020-09-10 18:55:25 +0000

    Create create_empty maplist function It uses 1 list comprehension and some fancy multiplication. SORRY VINCENT

--- radia/utils/maplist/__init__.py
+++ radia/utils/maplist/__init__.py
@@ -15,3 +15,19 @@ class Maplist:
     def __init__(self, pools: Pools, brackets: list):
         self.pools = pools
         self.brackets = brackets
+        self.maplist = self.create_empty()
+
+    def create_empty(self):
+        """Create an empty maplist object.
+        :returns: The empty structure of the object looks like this:
+            # [bracket[round[game{"map": str, "mode": str}]]
+        """
+        return [
+            [
+                [
+                    {"map": None, "mode": None}
+                ] * bracket["games"]
+            ] * bracket["rounds"]
+            for bracket in self.brackets
+        ]
+
