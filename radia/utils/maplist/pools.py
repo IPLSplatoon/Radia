7ad5554517e59d78d044bbb1ce0c618329b942e4
LeptoFlare <leptoflare@gmail.com>
2020-09-11 21:35:04 +0000

    Add more bags and pools... change how they are stored add some comments

--- radia/utils/maplist/pools.py
+++ radia/utils/maplist/pools.py
@@ -10,20 +10,25 @@ class Pools:
         """There aren't enough maps in the map pool."""
 
     def __init__(self, *, sz, tc, rm, cb) -> dict:
-        """ Create a dictionary representation of map pools.
-        :params: A list of :str maps:
-        :return dict: A dictionary with modes as keys and a list of :str maps: as values.
+        """ Create a class to represent map pools.
+        :params set: A :set: of :str maps:
+        :return dict: A dictionary with modes as keys and a Bag of :maps: as values.
             {"Splat Zones": [maps]}
         """
+        # Check if any map pool has less than the minimum allowed maps
         if any(len(pool) < self.MIN_MAPS for pool in [sz, tc, rm, cb]):
             raise self.NotEnoughMaps(f"Map pool must have {self.MIN_MAPS} or more maps.")
-
+        
+        # Create bags
         self.pools = {
-            "Splat Zones": sz,
-            "Tower Control": tc,
-            "Rainmaker": rm,
-            "Clam Blitz": cb,
+            "Splat Zones": Bag(sz),
+            "Tower Control": Bag(tc),
+            "Rainmaker": Bag(rm),
+            "Clam Blitz": Bag(cb),
         }
+        self.maps: Bag = Bag(self.__total_maps())
+        self.modes: Bag = Bag(set(self.pools.keys()))
+
 
     def total_maps(self):
         """Return the total number of maps in the pool."""
