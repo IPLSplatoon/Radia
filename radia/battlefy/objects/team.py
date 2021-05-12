be40e1f67edab4f0d1dc6e9973dc71c9035c993e
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2021-05-12 11:38:22 -0400

    Add bracket utilities to team object

--- radia/battlefy/objects/team.py
+++ radia/battlefy/objects/team.py
@@ -1,5 +1,9 @@
 """Battlefy team object."""
 
+from enum import Enum
+
+import discord
+from discord.ext import commands
 import dateutil.parser
 
 from .player import Player, Captain
@@ -12,6 +16,22 @@ class Team:
     :param str discord_field_id: The battlefy field id of the discord field
     :param str fc_field_id: The battlefy field id of the friend-code field
     """
+
+    class Bracket(Enum):
+        ALPHA = "a"
+        BETA = "b"
+        GAMMA = "g"
+
+        def role(self, ctx):
+            return discord.utils.get(ctx.guild.roles, name=f"LI {self.name.capitalize()}")
+
+        @classmethod
+        def find(cls, string):
+            for option in cls:
+                if string.lower().startswith(option.value):
+                    return option
+            else:
+                return cls(string)
 
     def __init__(self, battlefy, discord_field_id, fc_field_id):
         self.raw = battlefy
@@ -37,3 +57,21 @@ class Team:
             if field["_id"] == _id:
                 return field["value"]
         return default
+
+    async def assign_bracket(self, ctx, bracket):
+        """Assign a bracket to a team by captain roles."""
+        captain_discord = await self.captain.get_discord(ctx)
+        if captain_discord:
+            bracket_role = self.Bracket.find(bracket).role(ctx)
+            await captain_discord.add_roles(bracket_role)
+        else:
+            raise commands.BadArgument
+
+    async def get_bracket(self, ctx):
+        """Get the bracket of a team based on captain roles."""
+        captain_discord = await self.captain.get_discord(ctx)
+        if captain_discord:
+            for bracket in self.Bracket:
+                bracket_role = self.Bracket.find(bracket).role(ctx)
+                if bracket_role in captain_discord.roles:
+                    return bracket
